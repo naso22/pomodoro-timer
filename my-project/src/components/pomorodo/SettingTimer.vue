75c46dfdd3b3c22816e855b2b145ce41c2fde518
naso22 <naru.prog.0106@gmail.com>
2023-04-28 08:45:08 +0900

    setting画面の作成

---
+++ my-project/src/components/pomorodo/SettingTimer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <h1>Pomodoro Timer Settings</h1>
+        <table>
+            <tr>
+            <th >Work Time</th>
+                <td>
+            <input type="number" id="work-time" name="work-time" min="1" max="120" required>
+                </td>
+            </tr>
+
+            <tr>
+                <th >break Time</th>
+                <td>
+                    <input type="number" id="work-time" name="work-time" min="1" max="30" required>
+                </td>
+            </tr>
+
+            <tr>
+            <th >long break Time</th>
+            <td>
+                <input type="number" id="work-time" name="work-time" min="1" max="60" required>
+            </td>
+        </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+</script>
